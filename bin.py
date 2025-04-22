5c2757e86346a5010fa7466dd7311ab83f269150
Alec Zaitz <a02423683@usu.edu>
2025-04-21 18:40:13 -0600

    finished bin class

--- bin.py
+++ bin.py
@@ -2,18 +2,31 @@ class Bin:
     def __init__(self, capacity):
         self.items = []
         self.capacity = capacity
+        self.current_weight = 0
 
-    def get_sum(self):
+    def calc_curr_weight(self):
         items_sum = 0
         for item in self.items:
             items_sum += item
         return items_sum
 
     def is_full(self):
-        return self.get_sum() >= self.capacity
+        return self.current_weight >= self.capacity
+
+    def can_fit(self, item):
+        remainder = self.capacity - self.current_weight
+        return item <= remainder
 
     def add_item(self, item):
-        if self.capacity - self.get_sum() >= item:
+        if self.can_fit(item):
             self.items.append(item)
+            self.current_weight += item
         else:
-            raise Exception("Bin Overflow")
+            raise Exception("Tried to fit item where it can't")
+
+    def remove_item(self, item):
+        if item in self.items:
+            self.items.remove(item)
+            self.current_weight -= item
+        else:
+            raise Exception("Tried removing an item that wasn't in a bin")
--- exact.py
+++ exact.py
@@ -1,3 +1,18 @@
-def BinPackingExact(items, capacity):
-    """Exact Algorithm"""
+def bin_packing_exact(items, capacity):
+    def recursive_packing(bins, items, index):
+        """
+        Exact Algorithm - creates a recursive branch for
+        every scenario of where you place each item,
+        including in its own bin
+
+        Base case = index == len(items)
+
+        Making sure to backtrack and undo the placement
+        after each placement
+
+        Keep the best solution, found so far, and check if
+        it should be updated at the end of each recursion
+        """
+
+
     return None
